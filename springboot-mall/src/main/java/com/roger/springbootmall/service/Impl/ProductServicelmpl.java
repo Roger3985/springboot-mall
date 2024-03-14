7211347c9f5e7d4fa212c12ff713a9d53295ba0e
a39849686 <a0967067696@gmail.com>
2024-03-14 10:03:26 +0800

    「商品功能 - 實作修改商品」

--- springboot-mall/src/main/java/com/roger/springbootmall/service/Impl/ProductServicelmpl.java
+++ springboot-mall/src/main/java/com/roger/springbootmall/service/Impl/ProductServicelmpl.java
@@ -21,4 +21,9 @@ public class ProductServicelmpl implements ProductService {
     public Integer createProduct(ProductRequest productRequest) {
         return productDao.createProduct(productRequest);
     }
+
+    @Override
+    public void updateProduct(Integer productId, ProductRequest productRequest) {
+        productDao.updateProduct(productId, productRequest);
+    }
 }
